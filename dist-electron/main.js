6cbc0078dfa57f20d11b07aa894e0120cd35a3bb
Nicolas <nicosan12@hotmail.com>
2024-09-23 20:02:39 -0500

    remove top bar

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -21,6 +21,7 @@ function createWindow() {
     minWidth: 800,
     minHeight: 550
   });
+  win.setMenu(null);
   win.webContents.on("did-finish-load", () => {
     win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
   });
