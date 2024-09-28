c2b245a3c93276beba727d1020392d22274a47c6
Nicolas <nicosan12@hotmail.com>
2024-09-27 19:00:43 -0500

    storage service is added

--- src/lib/Interfaces/RequestModel.ts
+++ src/lib/Interfaces/RequestModel.ts
@@ -1,10 +1,16 @@
 import { HttpMethods } from "../Types/HttpMethods";
 
+export interface Requests {
+    requests: RequestModel[];
+}
+
 export interface RequestModel {
+    id: number,
     methodType: HttpMethods;
     url: string;
     body: string;
     hearders: Header[];
+    isSave: boolean;
 }
 
 export interface Header {
---
+++ src/services/StorageService.ts
@@ -0,0 +1,34 @@
+import { RequestModel } from '@/lib/Interfaces/RequestModel';
+import { Storage } from '../lib/Storage/Storage';
+import { LocalStorage } from '../lib/Storage/StorageManager';
+
+export class StorageService {
+
+    private requests: RequestModel[] = [];
+
+    constructor(private storage: Storage<RequestModel[]>) {
+        this.storage = new LocalStorage<RequestModel[]>();
+        this.requests = this.storage.getItem("requests") ?? [];
+    }
+
+    public saveRequests(request: RequestModel): void {
+        this.requests.push(request);
+        this.storage.setItem("requests", this.requests);
+    }
+
+    public getRequests(): RequestModel[] {
+        return [... this.requests];
+    }
+
+    public deleteRequest(request: RequestModel): void {
+        // Encuentra el índice de la request a eliminar usando el id
+        const index = this.requests.findIndex(req => req.id === request.id);
+
+        // Si se encuentra la request, elimínala de la lista
+        if (index !== -1) {
+            this.requests.splice(index, 1);
+            this.storage.setItem("requests", this.requests); // Actualiza el almacenamiento
+        }
+    }
+
+}
